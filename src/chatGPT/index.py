5f25293e954ec83499e31f79a6d91ac073747158
Francisco Galarza <galarzafrancisco98@gmail.com>
2024-04-03 08:48:19 -0300

    carga punto 4

--- src/chatGPT/index.py
+++ src/chatGPT/index.py
@@ -1,26 +1,32 @@
 import openai
+import sys
 
 # apikey
-openai.api_key = 'OPENAI_API_KEY'
+openai.api_key = 'API_KEY'
+
+# Variable para almacenar consultas
+bufferConsulta = []
 
 def obtener_respuesta(user_input):
-    # contexto y tatea
+    global bufferConsulta
+
+    # Dcontexto y tarea 
     context = "contexto del usuario"
     usertask = "tarea del usuario"
 
-    #verifica consulta
+    # verifica consulta
     if not user_input.strip():
         print("Por favor ingrese una consulta válida.")
         return
 
     try:
-        # llamada api
+        # Llamada API
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo-0125",
             messages=[
                 {"role": "system", "content": context},
                 {"role": "user", "content": usertask},
-                {"role": "user", "content": user_input}
+                {"role": "user", "content": user_input} 
             ],
             temperature=1,
             max_tokens=4096,
@@ -29,22 +35,32 @@ def obtener_respuesta(user_input):
             presence_penalty=0
         )
 
-        # respuesta gpt
+        # respuesta GPT
         print("chatGPT:", response.choices[0].message.content)
+        bufferConsulta.append(response.choices[0].message.content)
     except Exception as e:
-        print('error en la respuesta de chat gpt', e)
+        print('Error en la respuesta de chat GPT:', e)
 
-# ciclo para seguir preguntando
+# Verifica argumento --convers
+conversacion = '--convers' in sys.argv
+
+# Ciclo para seguir preguntando
 while True:
     try:
         user_input = input("You: ")
+
         if user_input.lower() == 'salir':
             break
 
         try:
             obtener_respuesta(user_input)
+
+            #si la conv esta activa y no se manda prompt, se usa la ultima consulta del q esta en el buffer
+
+            if conversacion and not user_input.strip() and bufferConsulta:
+                user_input = bufferConsulta[-1]
         except Exception as e:
-            print("error durante la consulta", e)
+            print("Error durante la consulta:", e)
 
     except Exception as e:
-        print("error en la entrada del ususario:", e)
+        print("Error en la entrada del usuario:", e)
