4d00ee46b99a3f5d52f0b90a5e30d281fdda5aa0
Francisco Galarza <galarzafrancisco98@gmail.com>
2024-04-03 07:39:05 -0300

    carga de punto 2

--- src/chatGPT/index.py
+++ src/chatGPT/index.py
@@ -1,7 +1,7 @@
 import openai
 
 # apikey
-openai.api_key = ''
+openai.api_key = 'OPENAI_API_KEY'
 
 def obtener_respuesta(user_input):
     # contexto y tatea
@@ -13,28 +13,38 @@ def obtener_respuesta(user_input):
         print("Por favor ingrese una consulta válida.")
         return
 
+    try:
+        # llamada api
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-0125",
+            messages=[
+                {"role": "system", "content": context},
+                {"role": "user", "content": usertask},
+                {"role": "user", "content": user_input}
+            ],
+            temperature=1,
+            max_tokens=4096,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
 
-    # llamada api
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-0125",
-        messages=[
-            {"role": "system", "content": context},
-            {"role": "user", "content": usertask},
-            {"role": "user", "content": user_input}
-        ],
-        temperature=1,
-        max_tokens=4096,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-
-    # respuesta gpt
-    print("chatGPT:", response.choices[0].message.content)
+        # respuesta gpt
+        print("chatGPT:", response.choices[0].message.content)
+    except Exception as e:
+        print('error en la respuesta de chat gpt', e)
 
 # ciclo para seguir preguntando
 while True:
-    user_input = input("You: ")
-    if user_input.lower() == 'salir':
-        break
-    obtener_respuesta(user_input)
+    try:
+        user_input = input("You: ")
+        if user_input.lower() == 'salir':
+            break
+
+        try:
+            obtener_respuesta(user_input)
+        except Exception as e:
+            print("error durante la consulta", e)
+
+    except Exception as e:
+        print("error en la entrada del ususario:", e)
