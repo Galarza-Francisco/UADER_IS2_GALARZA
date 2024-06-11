2562060ca55ac33d5be39c707544bec12d2b1425
Francisco Galarza <galarzafrancisco98@gmail.com>
2024-06-11 11:49:04 -0300

    aplicando singleton

--- src/TP6/RRR_TP6/RRR_TP1/getJasonNew.py
+++ src/TP6/RRR_TP6/RRR_TP1/getJasonNew.py
@@ -1,4 +1,4 @@
-import json
+"""import json
 import sys
 
 
@@ -39,3 +39,52 @@ def main():
 
 if __name__ == "__main__":
     main()
+    """
+import json
+import sys
+
+class recuperadorToken:
+    _instance = None  # Variable de clase para almacenar la instancia única
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(recuperadorToken, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self, jsonfile):
+        # Evitar la re-inicialización de la instancia única
+        if not hasattr(self, 'initialized'):
+            self.jsonfile = jsonfile
+            self.data = self.load_json()
+            self.initialized = True
+
+    def load_json(self):
+        try:
+            with open(self.jsonfile, 'r') as myfile:
+                data = myfile.read()
+            return json.loads(data)
+        except FileNotFoundError:
+            print(f"File not found: {self.jsonfile}")
+            sys.exit(1)
+        except json.JSONDecodeError:
+            print(f"Error decoding JSON from file: {self.jsonfile}")
+            sys.exit(1)
+
+    def get_token(self, jsonkey):
+        return self.data.get(jsonkey, "Key not found")
+
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: python getJasonNew.py <jsonfile> <jsonkey>")
+        return
+
+    jsonfile = sys.argv[1]
+    jsonkey = sys.argv[2]
+
+    recuperado = recuperadorToken(jsonfile)
+    token = recuperado.get_token(jsonkey)
+    print("Token: ", token)
+
+if __name__ == "__main__":
+    main()
+
